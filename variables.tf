b99a110bf04b3dc725ef580c3165e14227143de5
d4n13l-4lf4 <d4n13l.4lf4@gmail.com>
2023-12-07 16:21:39 -0500

    Remove go 1.20 from testing matrix and remove unused tf vars

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "tf_organization" {
-    description = "Terraform cloud organization"
-    type = string
-}
-
-variable "tf_workspace" {
-    description = "Terraform cloud workspace"
-    type = string
-}
