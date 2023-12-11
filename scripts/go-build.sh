d2711f34e67d29412e1a48ad3cea3e696602c434
d4n13l-4lf4 <d4n13l.4lf4@gmail.com>
2023-12-10 23:12:29 -0500

    Use bash as shell for makefile

--- scripts/go-build.sh
+++ scripts/go-build.sh
@@ -28,11 +28,15 @@ done
 CMD_DIR="${CMD_DIR:-cmd}"
 OUT_DIR="${OUT_DIR:-build}"
 
+echo $CMD_DIR
+echo $OUT_DIR
+echo $PWD
+
 # Erase output directory
 rm -rf $PWD/$OUT_DIR
 
 # Compile handlers
-find "$PWD/$CMD_DIR" -type "file" -name "*.go" -print0 | while read -d $'\0' file
+find "$PWD/$CMD_DIR" -type 'f' -name "*.go" -print0 | while read -d $'\0' file
 do
     echo Compiling $file
     DIR_NAME=$(basename $(dirname $file))
