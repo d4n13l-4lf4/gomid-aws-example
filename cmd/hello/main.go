3ab19ac066b7790a63d836422d498345af3547af
d4n13l-4lf4 <d4n13l.4lf4@gmail.com>
2023-12-11 16:21:06 -0500

    Fix handler and api gateway permissions

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -7,21 +7,15 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/d4n13l-4lf4/gomid-aws-example/auth"
 	"github.com/d4n13l-4lf4/gomid-aws-example/hello"
-	httpLambda "github.com/d4n13l-4lf4/gomid-aws-example/http"
 	"github.com/d4n13l-4lf4/gomid/middleware"
 )
 
 // nolint: gochecknoglobals
 var allowedUsers = []string{"d4n13l-4lf4"}
 
-type (
-	// Greet greeting controller type.
-	Greet func(context.Context, *events.APIGatewayProxyRequest) (*httpLambda.LambdaResponse[string], error)
-)
-
 func main() {
 	greeter := hello.NewGreetingController(hello.Greet)
-	chain := middleware.Wrap[Greet](
+	chain := middleware.Wrap[func(context.Context, *events.APIGatewayProxyRequest) (any, error)](
 		greeter.Greet,
 	).
 		Add(auth.AuthenticateUser(allowedUsers)).
